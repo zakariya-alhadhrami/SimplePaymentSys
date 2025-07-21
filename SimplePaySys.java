3d3d880dba2bee498bf97684b1c4181550e52985
Zakariya AlHadhrami <zakarya601@gmail.com>
2025-07-21 15:31:37 +0400

    feat: adding the new class creditCard that extends the payment class and also overriding the pay method

--- SimplePaySys.java
+++ SimplePaySys.java
@@ -13,6 +13,19 @@ public class SimplePaySys {
         public abstract void pay(double amount);
     }
 
+    public class CreditCard extends PaymentMethod{
+
+        public CreditCard(String name){
+            super(name);
+        }
+
+        @Override
+        public void pay(double amount){
+            System.out.println("Paid"+ amount+ "using Credit Card: "+ getName());
+        }
+
+    }
+
     public static void main(String[] args) {
         System.out.println("hello world");
     }
