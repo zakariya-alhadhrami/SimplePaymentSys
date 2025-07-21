cda93ca4ad3c21f1f1885d00bcbda960b804eb5f
Zakariya AlHadhrami <zakarya601@gmail.com>
2025-07-21 15:17:39 +0400

    feat: added the abstract class paymentmethod with the private string with the seter and geter methods along with the abstract method

--- SimplePaySys.java
+++ SimplePaySys.java
@@ -1,5 +1,18 @@
 public class SimplePaySys {
 
+    public abstract class PaymentMethod{
+        private String name;
+
+        public PaymentMethod(String name){
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+        public abstract void pay(double amount);
+    }
+
     public static void main(String[] args) {
         System.out.println("hello world");
     }
